795b231cf37cb601e3352f2d70c2f0587c8012f0
michelfigi <micheldascse01@gmail.com>
2022-08-02 13:11:48 +0900

    Initial Commit

---
+++ Assets/AudioSystem/Pool/Example/ParticleFactorySO.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using SoundSystem.Factory;
+
+[CreateAssetMenu(fileName = "NewParticleFactory", menuName = "Factory/Particle Factory")]
+public class ParticleFactorySO : FactorySO<ParticleSystem>
+{
+	public override ParticleSystem Create()
+	{
+		return new GameObject("ParticleSystem").AddComponent<ParticleSystem>();
+	}
+}
+
